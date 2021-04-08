a28176adcf364f95e975e1c8218a599d9f15e788
nikitaco <82123863+nikitaco@users.noreply.github.com>
2021-04-08 16:25:05 -0400

    Add files via upload

---
+++ Weather/Model/WeatherData.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
